50974c6ca4b911c7c0fd5a624b7afe071c4b1d43
shota nakaguchi <hare.tokidoki.kumori227@gmail.com>
2019-02-18 22:32:25 +0900

    editdestroy

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -19,4 +19,24 @@ class UploadsController < ApplicationController
         render("uploads/new")
       end
   end
+
+  def edit
+     @upload = Upload.find_by(id: params[:id])
+  end
+
+  def update
+    @upload = Upload.find_by(id:params[:id])
+    @upload.comment = params[:comment]
+    @upload.save
+     redirect_to("/uploads/index")
+  end
+
+  def destroy
+
+    @upload = Upload.find_by(id:params[:id])
+    @upload.destroy
+
+     redirect_to("/uploads/index")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "uploads/index" => "uploads#index"
   get "uploads/new" => "uploads#new"
   post "uploads/create" => "uploads#create"
+  get "uploads/:id/edit" => "uploads#edit"
+  post "uploads/:id/update" => "uploads#update"
+  post "uploads/:id/destroy" => "uploads#destroy"
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
