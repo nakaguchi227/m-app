f90d72a755087b727888fae157f7d3edd99401b2
shota nakaguchi <hare.tokidoki.kumori227@gmail.com>
2019-02-27 09:08:18 +0900

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "users/new" => "users#new"
   get "login" => "users#login_form"
   post "login" => "users#login"
-  post "logout" => "users#logout"
+  delete "logout" => "users#logout"
   get "users/index" => "users#index"
   post "users/create" => "users#create"
   get "uploads/index" => "uploads#index"
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post "uploads/create" => "uploads#create"
   get "uploads/:id/edit" => "uploads#edit"
   post "uploads/:id/update" => "uploads#update"
-  post "uploads/:id/destroy" => "uploads#destroy"
+  delete "uploads/:id/destroy" => "uploads#destroy"
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
